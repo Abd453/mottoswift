303f05933b9c440bd0a8ff3571a37598f99edf4a
Abd453 <134734720+Abd453@users.noreply.github.com>
2024-11-05 22:39:45 +0300

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/carl.jpg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/carl.jpg';
+import viteLogo from '/vite.svg';
+import Header from './Landing/Header';
+import carl from './assets/carl.jpg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      
-      <h1>Vite + React</h1>
+      {/* <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-       
-      </div>
-      
+      </div> */}
+      <img src={carl} alt="Carl" />
+      {/* <Header /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
